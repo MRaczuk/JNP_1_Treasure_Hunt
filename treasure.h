25f9b89b31c6cebb0ae6d4e2a6c79d3b98e2fb9e
Laplasjan107 <67481570+Laplasjan107@users.noreply.github.com>
2021-12-04 23:44:14 +0100

    usunięto specjalizację

--- treasure.h
+++ treasure.h
@@ -4,49 +4,36 @@
 #include <concepts>
 
 template<std::integral ValueType, bool IsTrapped>
-class Treasure;
-
-template<std::integral ValueType>
-class Treasure<ValueType, true> {
+class Treasure {
 private:
     ValueType value;
 
 public:
-    static const bool isTrapped = true;
-    constexpr Treasure (ValueType value) : value(value) {}
-    constexpr ValueType evaluate() const {
-        return value;
-    }
-    constexpr ValueType getLoot(){
-        ValueType out = this->value;
-        this->value = 0;
-        return out;
-    }
+    static constexpr const bool isTrapped = IsTrapped;
+    constexpr Treasure (ValueType value);
+    constexpr ValueType evaluate() const;
+    constexpr ValueType getLoot();
 };
 
-template<std::integral ValueType>
-class Treasure<ValueType, false> {
-private:
-    ValueType value;
+template <std::integral ValueType, bool IsTrapped>
+constexpr Treasure<ValueType, IsTrapped> :: Treasure(ValueType value) : value(value), isTrapped(IsTrapped) {};
 
-public:
-    static const bool isTrapped = false;
-    constexpr Treasure (ValueType value) : value(value) {}
-    constexpr ValueType evaluate() const {
-        return value;
-    }
-    constexpr ValueType getLoot(){
-        ValueType out = this->value;
-        this->value = 0;
-        return out;
-    }
-};
+template <integral ValueType, bool IsTrapped>
+constexpr ValueType Treasure<ValueType, IsTrapped> :: evaluate() const {
+    return value;
+}
 
+template <integral ValueType, bool IsTrapped>
+constexpr ValueType Treasure<ValueType, IsTrapped> :: getLoot() {
+    ValueType out = this->value;
+    this->value = 0;
+    return out;
+}
 
-template<std::integral ValueType>
+template<integral ValueType>
 using SafeTreasure = Treasure<ValueType, false>;
 
-template<std::integral ValueType>
+template<integral ValueType>
 using TrappedTreasure = Treasure<ValueType, true>;
 
 #endif // TREASURE_H
